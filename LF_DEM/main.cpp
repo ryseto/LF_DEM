24bc6e7e611ebdfd776d1e80c82c7405d3f19735
Ryohei Seto <seto@lipc18.engr.ccny.cuny.edu>
2012-11-14 16:15:33 -0500

    Initial Commit

---
+++ LF_DEM/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LF_DEM
+//
+//  Created by Ryohei Seto on 11/14/12.
+//  Copyright (c) 2012 Ryohei Seto. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
