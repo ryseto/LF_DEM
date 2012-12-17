15078a75f1e4ab8eb40be84e4886340ce66dc999
Romain Mari <romain.mari@gmail.com>
2012-12-17 13:34:37 -0500

    Bug fix on the Brownian forces.

--- LF_DEM/Simulation.cpp
+++ LF_DEM/Simulation.cpp
@@ -16,7 +16,7 @@ Simulation::~Simulation(){
 
 void Simulation::SetParameters(int argc, const char * argv[]){
 	sys.lub = true;
-	sys.brownian = false;
+	sys.brownian = true;
 	filename_import_positions = argv[1];
 	sys.lubcore = atof(argv[2]);
 	string arg3 =argv[3];
@@ -75,7 +75,7 @@ void Simulation::SetParameters(int argc, const char * argv[]){
 	/*
 	 * Temperature
 	 */
-	sys.kb_T=0.01;
+	sys.kb_T=1.0;
 	/*
 	 * Simulation terminate at this value
 	 */
@@ -84,7 +84,7 @@ void Simulation::SetParameters(int argc, const char * argv[]){
 	 * dt: the time step to integrate the equation of motion.
 	 * We need to give a good criterion to give.
 	 */
-	sys.dt = 1e-4 / sys.shear_rate;
+	sys.dt = 1e-5 / sys.shear_rate;
 	/* 
 	 * dt_mid: the intermediate time step for the mid-point 
 	 * algortithm. dt/dt_mid = dt_ratio
@@ -185,6 +185,8 @@ void Simulation::SimulationMain(int argc, const char * argv[]){
 	}
 
 	sys.checkNewInteraction();
+	//	int count = 0;
+	//	while(count++<3){
 	while(true){
 		sys.timeEvolution(interval_snapshot);
 		outputRheologyData();
