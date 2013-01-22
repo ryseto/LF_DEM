dd7787012d6359ceb79041445c0001814d0b8a1d
Ryohei Seto <setoryohei@me.com>
2013-01-22 12:21:46 -0500

    Modified generateYaplotFile.

--- LF_DEM/generateYaplotFile.pl
+++ LF_DEM/generateYaplotFile.pl
@@ -6,56 +6,70 @@ $force_factor = $ARGV[2];
 open (IN_particle, "< ${particle_data}");
 open (IN_interaction, "< ${interaction_data}");
 
+&readHeader;
 # Read the header
 #
-$line = <IN_particle>; ($buf, $VF) = split(/\s+/, $line);
-$line = <IN_particle>; ($buf, $Lx) = split(/\s+/, $line);
-$line = <IN_particle>; ($buf, $Ly) = split(/\s+/, $line);
-$line = <IN_particle>; ($buf, $Lz) = split(/\s+/, $line);
-$line = <IN_particle>; ($buf, $np_a) = split(/\s+/, $line);
-$line = <IN_particle>; ($buf, $np_b) = split(/\s+/, $line);
-$line = <IN_particle>; ($buf, $radius_a) = split(/\s+/, $line);
-$line = <IN_particle>; ($buf, $radius_b) = split(/\s+/, $line);
-printf "$VF, $Lx, $Ly, $Lz, $np_a, $np_b, $radius_a, $radius_b\n";
-
 # Create output file name
 $i = index($particle_data, 'par_', 0)+4;
 $j = index($particle_data, '.dat', $i-1);
 $name = substr($particle_data, $i, $j-$i);
 $output = "$name.yap";
 open (OUT, "> ${output}");
-
 $np = $np_a + $np_b;
 while ( 1 ){
-    $line = <IN_particle>; 
+	&InParticles;
+	&InInteractions;
+	&OutYaplotData;
+}
+close (OUT);
+
+sub readHeader{
+	$line = <IN_particle>; ($buf, $VF) = split(/\s+/, $line);
+	$line = <IN_particle>; ($buf, $Lx) = split(/\s+/, $line);
+	$line = <IN_particle>; ($buf, $Ly) = split(/\s+/, $line);
+	$line = <IN_particle>; ($buf, $Lz) = split(/\s+/, $line);
+	$line = <IN_particle>; ($buf, $np_a) = split(/\s+/, $line);
+	$line = <IN_particle>; ($buf, $np_b) = split(/\s+/, $line);
+	$line = <IN_particle>; ($buf, $radius_a) = split(/\s+/, $line);
+	$line = <IN_particle>; ($buf, $radius_b) = split(/\s+/, $line);
+	printf "$VF, $Lx, $Ly, $Lz, $np_a, $np_b, $radius_a, $radius_b\n";
+}
+
+sub InParticles {
+	$line = <IN_particle>;
     ($buf, $shear_rate) = split(/\s+/, $line);
+	if ($buf != "#"){
+        exit;
+    }
     printf "$shear_rate\n";
     for ($i = 0; $i < $np; $i ++){
         $line = <IN_particle> ;
         ($i, $x, $y, $z, $vx, $vy, $vz, $ox, $oy, $oz ) = split(/\s+/, $line);
-        $posx[$i] = $x; 
-        $posy[$i] = $y; 
+        $posx[$i] = $x;
+        $posy[$i] = $y;
         $posz[$i] = $z;
     }
-    if ($buf != "#"){
-        exit;
-    }
+}
 
-    $line = <IN_interaction>; 
-    ($buf, $shear_rate, $num_interaction) = split(/\s+/, $line);
-    if ($buf != "#"){
-        exit;
-    }
-    for ($k = 0; $k < $num_interaction; $k ++){
-        $line = <IN_interaction> ;
-        ($i, $j, $r, $f_lub, $fc_n, $fc_t, $fric_st) = split(/\s+/, $line);
-        $int0[$k] = $i;
-        $int1[$k] = $j;
-        $F_lub[$k] = $f_lub;
-        $Fc_n[$k] = $fc_n;
-        $Ft_t[$k] = $fc_t;
-    }
-    printf OUT "y 1\n";
+sub InInteractions {
+	$line = <IN_interaction>;
+	($buf, $shear_rate, $num_interaction) = split(/\s+/, $line);
+	if ($buf != "#"){
+		exit;
+	}
+	for ($k = 0; $k < $num_interaction; $k ++){
+		$line = <IN_interaction> ;
+		($i, $j, $r, $f_lub, $fc_n, $fc_t, $fric_st) = split(/\s+/, $line);
+		$int0[$k] = $i;
+		$int1[$k] = $j;
+		$F_lub[$k] = $f_lub;
+		$Fc_n[$k] = $fc_n;
+		$Ft_t[$k] = $fc_t;
+	}
+}
+
+sub OutYaplotData{
+	printf OUT "y 1\n";
     printf OUT "@ 2\n";
     for ($i = 0; $i < $np; $i ++){
         printf OUT "c $posx[$i] $posy[$i] $posz[$i] \n";
@@ -76,27 +90,25 @@ while ( 1 ){
         if ($force > 0){
             $string_with = ${force_factor}*$force;
             printf OUT "r ${string_with}\n";
-            OutString($int0[$k],  $int1[$k]);
-        } 
-    }    
+            &OutString($int0[$k],  $int1[$k]);
+        }
+    }
     printf OUT "@ 4\n";
     for ($k = 0; $k < $num_interaction; $k ++){
         $force = $F_lub[$k] + $Fc_n[$k];
         if ($force < 0){
             $string_with = -${force_factor}*$force;
             printf OUT "r ${string_with}\n";
-            OutString($int0[$k],  $int1[$k]);
-        } 
+            &OutString($int0[$k],  $int1[$k]);
+        }
     }
     printf OUT "\n";
 }
-close (OUT);
-exit;    
 
 sub OutString {
     ($i, $j) = @_;
     $xi = $posx[$i];
-    $yi = $posy[$i]; 
+    $yi = $posy[$i];
     $zi = $posz[$i];
     $xj = $posx[$j];
     $yj = $posy[$j]; 
