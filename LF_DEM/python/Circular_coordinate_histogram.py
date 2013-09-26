fe76d3789df6183bc4abbadfeafb2809c162e77c
Romain Mari <romain.mari@gmail.com>
2013-09-26 17:27:09 -0400

    Heavy reworking of the scripts for pair correlation function
    Now everything is vectorized with numpy, which is much faster than
    before.

---
+++ LF_DEM/python/Circular_coordinate_histogram.py
@@ -0,0 +1,101 @@
+#!/opt/local/Library/Frameworks/Python.framework/Versions/2.7/bin/python
+#coding=utf-8
+
+import sys
+import math
+import pycart2sph
+import numpy as np
+import string
+
+class CircularCoordinateHistogram:
+
+    def __init__(self, r_bn, theta_bn, min_r, max_r, is_sym):
+        self.r_bin_nb=r_bn
+        self.theta_bin_nb=theta_bn
+
+        self.histogram = np.zeros((r_bn, theta_bn))
+
+        self.r_max=max_r
+        self.r_min=min_r
+
+        self.pi=np.arccos(-1)
+        self.r_bsize=(self.r_max-self.r_min)/self.r_bin_nb
+        
+        if is_sym:
+            self.theta_max = self.pi # symmetry: only theta in [0:pi]
+        else:
+            self.theta_max = 2*self.pi # symmetry: only theta in [0:pi]
+
+        self.theta_bsize=self.theta_max/self.theta_bin_nb
+
+
+    def update(self, coord, value, coord_sys='circ', act='add'):
+
+        if coord_sys == 'cart':
+            (r, theta)=pycart2sph.cart2circ_nolist(coord)
+        else:
+            coordarray = np.array(coord)
+            r = coordarray[:,0]
+            theta = coordarray[:,1]
+
+
+        r_bin = np.array((r-self.r_min)/self.r_bsize, dtype=int)
+        theta_bin = np.array(theta/self.theta_bsize, dtype=int)
+
+        inrange_bin = np.logical_and(r_bin<self.r_bin_nb, r_bin>=0)
+
+        r_bin = r_bin[ inrange_bin ]
+        theta_bin = theta_bin[ inrange_bin ]
+        values = np.array(value)[ inrange_bin ]
+
+        if act == 'add':
+            # 'add' is a bit tricky, 
+            # because treats a[indices] += x in a somewhat unexpected way:
+            # it ignores duplicates in the indices
+            # the solution here is from:
+            # http://stackoverflow.com/questions/18273634/assigning-identical-array-indices-at-once-in-python-numpy
+            # the idea is to use bincount to aggregate the duplicate indices first, and then feed the array with an array of unique indices. 
+            # but, as things cannot be simple, bincount can only aggregate 1d indices, 
+            # so there's a pre-step to convert 2d indices in 1d, with ravel_multi_index
+            
+            flat_indices = np.ravel_multi_index((r_bin, theta_bin), dims=self.histogram.shape)
+            unique_indices = np.unique(flat_indices)
+            aggregated_values = (np.bincount(flat_indices, weights = values))[unique_indices]
+            self.histogram[np.unravel_index(unique_indices, dims=self.histogram.shape)] += aggregated_values
+
+        elif act == 'replace':
+            self.histogram[r_bin, theta_bin] = values
+        else:
+            sys.stderr.write("  CircularCoordinateHistogram :: update(pos, value, coord='sph',act='add') :\n       Unknown act %s".str(act))
+            sys.exit(1)
+
+
+    def normalize(self, norm_factor=1): # for g(r), norm_factor = V*nb_of_snapshots
+        for i in range(self.r_bin_nb):
+            r=self.r_bsize*(i+0.5)+self.r_min
+            rlo=self.r_bsize*i+self.r_min
+            for j in range(self.theta_bin_nb):
+                theta=self.theta_bsize*(j+0.5)
+                dvol=rlo*self.theta_bsize*self.r_bsize
+                self.histogram[i,j]/=dvol*norm_factor
+
+#    def bin_coord(self,bins):
+#        r=self.r_bsize*(float(i)+0.5)+self.r_min
+#        theta=self.theta_bsize*(float(j)+0.5)
+#        return (r, theta)
+    
+    def print_to(self, stream):
+        headline=str(self.r_bin_nb)+' '+str(self.theta_bin_nb)+' '+str(self.r_max)+'\n'
+        stream.write(headline)
+
+        for i in range(self.r_bin_nb):
+            r=self.r_bsize*(i+0.5)+self.r_min
+            for j in range(self.theta_bin_nb):
+                theta=self.theta_bsize*(j+0.5)
+#                out_string=str(r)+" "+str(theta)+" "+str(phi)+" "+" ".join(map(str, self.histogram[point]))+'\n'
+                out_string=str(r)+" "+str(theta)+" "+str(self.histogram[i,j])+'\n'
+                stream.write(out_string)
+
+
+
+    
--- LF_DEM/python/pyLF_DEM_posfile_reading.py
+++ LF_DEM/python/pyLF_DEM_posfile_reading.py
@@ -59,19 +59,19 @@ class Pos_Stream:
         else:
             self.lz = float(fields[2])
 
-        if self.Ly() == 0.: # 2d case
-            self.V = self.Lx()*self.Lz()
+        if self.ly == 0.: # 2d case
+            self.V = self.lx*self.lz
             self.dim=2
         else:               # 3d case
-            self.V = self.Lx()*self.Ly()*self.Lz()
+            self.V = self.lx*self.ly*self.lz
             self.dim=3
 
         self.reset_members()
 
     def reset_members(self):
-        self.positions_dict=dict()
-        self.old_positions=dict()
-        self.radius_dict=dict()
+        self.positions = np.zeros((self.N, 3))
+        self.old_positions = np.zeros((self.N, 3))
+        self.radius = np.zeros(self.N)
         self.last_read_time_label=[]
         self.time_labels=[]
         self.current_time_index=0
@@ -81,14 +81,6 @@ class Pos_Stream:
             print "Invalid input "
             sys.exit(1)
         
-    def positions_copy(self):
-        return self.positions.copy()
-
-    def positions_deepcopy(self):
-        return copy.deepcopy(self.positions)
-
-
-
 
     def update_labels(self, tlabel):
         if len(self.time_labels) > 0 :
@@ -118,16 +110,14 @@ class Pos_Stream:
             if len(values) > 10 : # exact number to be fixed. Now it is different in 2d an 3d, due to different output of angular position
                 i=int(values[0])
 #                self.positions[i]=[float(values[j])+0.5*self.cell_size for j in range(1,4)]
-                self.positions_dict[i]=[float(values[j]) for j in range(2,5)]
-                self.radius_dict[i]=float(values[1])
+                self.positions[i,:] = np.array([values[j] for j in range(2,5)], dtype=float)
+                self.radius[i] = float(values[1])
 
         return switch
 
 
     def get_snapshot(self, verbose=False):
 
-#        self.old_positions=self.positions.copy()
-        self.positions_dict.clear()
         switch=0
 
         count=0
@@ -142,7 +132,6 @@ class Pos_Stream:
             switch+=self.convert_input_pos(input_line)
 
             if switch==1:
-                self.positions = np.asarray(self.positions_dict.values())
                 return True
 
             
@@ -153,182 +142,78 @@ class Pos_Stream:
         deltax = delta[0]
         deltay = delta[1]
         deltaz = delta[2]
-        xshift = (self.time()-int(self.time()))*self.Lx()
+        xshift = (self.time()-int(self.time()))*self.lx
 
-        if deltaz > 0.5*self.Lz():
-            deltaz = deltaz - self.Lz()
+        if deltaz > 0.5*self.lz:
+            deltaz = deltaz - self.lz
             deltax = deltax - xshift
         else:
-            if deltaz < -0.5*self.Lz():
-                deltaz = deltaz + self.Lz()
+            if deltaz < -0.5*self.lz:
+                deltaz = deltaz + self.lz
                 deltax = deltax + xshift
                 
-        if deltay > 0.5*self.Ly():
-            deltay = deltay - self.Ly()
+        if deltay > 0.5*self.ly:
+            deltay = deltay - self.ly
         else:
-            if deltay < -0.5*self.Ly():
-                deltay = deltay + self.Ly()
+            if deltay < -0.5*self.ly:
+                deltay = deltay + self.ly
 
-        if deltax > 0.5*self.Lx():
-            deltax = deltax - self.Lx()
+        if deltax > 0.5*self.lx:
+            deltax = deltax - self.lx
         else:
-            if deltax < -0.5*self.Lx():
-                deltax = deltax + self.Lx()
+            if deltax < -0.5*self.lx:
+                deltax = deltax + self.lx
 
         return [deltax, deltay, deltaz]
 
-    def pos(self,i):
-        i=str(i)
-        return self.positions_dict[i]
-
-    def rad(self,i):
-        i=str(i)
-        return self.radius_dict[i]
-
-
 
     def computePairSeparations(self):
-        flat_pos = np.ravel(self.positions)
-        self.pair_sep = np.asarray([ flat_pos - np.roll(flat_pos, 3*i, axis=0) for i in self.range()] )
+        self.pair_sep = self.positions[:,None,:] - self.positions
 
         # define the shifts to apply if a boundary is crossed
-        xcross_shift = np.tile([self.Lx(), 0, 0], self.np()) # x
-        ycross_shift = np.tile([0, self.Ly(), 0], self.np()) # y
-        xshift = (self.time()-int(self.time()))*self.Lx()    # z (Lees-Edwards)
-        zcross_shift = np.tile([xshift, 0, self.Lz()], (self.np(),self.np())) # z
+        xshift = (self.time()-int(self.time()))*self.lx    # z (Lees-Edwards)
 
         # easier to start by delta_z
 
         # case delta_z > 0.5*Lz:
         # determine which elements have to be updated:
-        # if delta_z > 0.5*Lz, delta_x and delta_z should be updated, delta_y remains
-        # so the 3 entries will appear in the mask as [ True, False, True ]
-        zcrossed = np.repeat(self.pair_sep[:,2::3] > 0.5*self.Lz(),2, axis=1) #  [ True, True ]
-#        mask = np.insert(zcrossed, np.tile(np.arange(1,2*zcrossed.shape[0],2), (200, 1)), False, axis=1) # insert False in between
-        mask = np.insert(zcrossed, np.arange(1,2*zcrossed.shape[0],2), False, axis=1) # insert False in between
-        # replace values using the mask
-        self.pair_sep = np.where(mask, self.pair_sep-zcross_shift, self.pair_sep)
+        crossed = self.pair_sep[:,:,2] > 0.5*self.lz
+        # shift values
+        self.pair_sep = np.where(crossed[:,:,None], self.pair_sep-[xshift, 0, self.lz], self.pair_sep)
         
         # case delta_z < -0.5*Lz:
         # same as above
-        zcrossed = np.repeat(self.pair_sep[:,2::3] < -0.5*self.Lz(),2, axis=1)
-#        mask = np.insert(zcrossed, np.tile(np.arange(1,2*zcrossed.shape[0],2), (200, 1)), False, axis=1)
-        mask = np.insert(zcrossed, np.arange(1,2*zcrossed.shape[0],2), False, axis=1)
-        # replace values using the mask
-        self.pair_sep = np.where(mask, self.pair_sep+zcross_shift, self.pair_sep)
-        
+        crossed = self.pair_sep[:,:,2] < -0.5*self.lz
+        # shift values
+        self.pair_sep = np.where(crossed[:,:,None], self.pair_sep+[xshift, 0, self.lz], self.pair_sep)
 
         # case delta_x > 0.5*Lx:
-        # delta_x alone should be updated, mask [ True, False, False ]
-        mask = np.tile(np.asarray(np.zeros(2*self.np()), dtype=bool), (200, 1)) # [ False, False ]
-#        mask = np.insert(mask, np.tile(np.arange(0,2*mask.shape[0],2), (200,1)), self.pair_sep[:,0::3] > 0.5*self.Lx(), axis=1) # [ True, False, False ]
-        mask = np.insert(mask, np.arange(0,2*mask.shape[0],2), self.pair_sep[:,0::3] > 0.5*self.Lx(), axis=1) # [ True, False, False ]
-        # replace values using the mask
-        self.pair_sep = np.where(mask, self.pair_sep-xcross_shift, self.pair_sep)
+        crossed = self.pair_sep[:,:,0] > 0.5*self.lx
+        self.pair_sep = np.where(crossed[:,:,None], self.pair_sep-[self.lx, 0, 0], self.pair_sep)
 
         # case delta_x < -0.5*Lx:
-        mask = np.tile(np.asarray(np.zeros(2*self.np()), dtype=bool), (200, 1)) # [ False, False ]
-#        mask = np.insert(mask, np.tile(np.arange(0,2*len(mask),2), (200,1)), self.pair_sep[:,0::3] < -0.5*self.Lx(), axis=1) # [ True, False, False ]
-        mask = np.insert(mask, np.arange(0,2*len(mask),2), self.pair_sep[:,0::3] < -0.5*self.Lx(), axis=1) # [ True, False, False ]
-        # replace values using the mask
-        self.pair_sep = np.where(mask, self.pair_sep+xcross_shift, self.pair_sep)
+        crossed = self.pair_sep[:,:,0] < -0.5*self.lx
+        self.pair_sep = np.where(crossed[:,:,None], self.pair_sep+[self.lx, 0, 0], self.pair_sep)
 
+        # case delta_x > 0.5*Lx:
+        crossed = self.pair_sep[:,:,1] > 0.5*self.ly
+        self.pair_sep = np.where(crossed[:,:,None], self.pair_sep-[0, self.ly, 0], self.pair_sep)
 
-        # case delta_y > 0.5*Ly:
-        # delta_y alone should be updated, mask [ False, True, False ]
-        mask = np.tile(np.asarray(np.zeros(2*self.np()), dtype=bool), (200, 1)) # [ False, False ]
-#        mask = np.insert(mask, np.tile(np.arange(0,2*len(mask),2), (200,1)), self.pair_sep[:,1::3] > 0.5*self.Ly(), axis=1) # [ False, True, False ]
-        mask = np.insert(mask, np.arange(0,2*len(mask),2), self.pair_sep[:,1::3] > 0.5*self.Ly(), axis=1) # [ False, True, False ]
-        # replace values using the mask
-        self.pair_sep = np.where(mask, self.pair_sep-ycross_shift, self.pair_sep)
+        # case delta_x < -0.5*Lx:
+        crossed = self.pair_sep[:,:,1] < -0.5*self.ly
+        self.pair_sep = np.where(crossed[:,:,None], self.pair_sep+[0, self.ly, 0], self.pair_sep)
 
-        # case delta_y < -0.5*Ly:
-        # delta_y alone should be updated, mask [ False, True, False ]
-        mask = np.tile(np.asarray(np.zeros(2*self.np()), dtype=bool), (200, 1)) # [ False, False ]
-#        mask = np.insert(mask, np.tile(np.arange(0,2*len(mask),2), (200,1)), self.pair_sep[:,1::3] < -0.5*self.Ly(), axis=1) # [ False, True, False ]
-        mask = np.insert(mask, np.arange(0,2*len(mask),2), self.pair_sep[:,1::3] < -0.5*self.Ly(), axis=1) # [ False, True, False ]
-        # replace values using the mask
-        self.pair_sep = np.where(mask, self.pair_sep+ycross_shift, self.pair_sep)
-
-        self.pair_sep = np.reshape(self.pair_sep, (self.np(), self.np(), 3))
-        
-        self.pair_dist = np.sum(self.pair_sep**2, axis=2)**(1./2)
+        self.pair_dist = np.sqrt((self.pair_sep**2).sum(axis=2))
 
 
     def periodized_pos(self,i):
-        i=str(i)
-        return self.periodize(self.positions_dict[i])
+        return self.periodize(self.positions[i])
 
-
-    def pos_diff_vec(self,a,b):
-
-        
-        deltax=a[0]-b[0]
-        deltay=a[1]-b[1]
-        deltaz=a[2]-b[2]
-        print "pos ", a
-        print "pos ", b
-        print [deltax, deltay, deltaz]
-        [deltax, deltay, deltaz]=self.periodize([deltax, deltay, deltaz])
-        print [deltax, deltay, deltaz]
-        deltatot= math.sqrt(deltax*deltax+deltay*deltay+deltaz*deltaz)
-        return [deltax, deltay, deltaz, deltatot]
-    
-    def pos_diff(self,i,j):
-        i=str(i)
-        j=str(j)
-
-        return self.pos_diff_vec(self.positions_dict[i],self.positions_dict[j])
 
 
     def displacement(self, i):
-        dr=self.pos_diff_vec(self.positions_dict[i],self.old_positions_dict[i])
+        dr=self.pos_diff_vec(self.positions[i],self.old_positions[i])
         return dr
-
-    def non_affine_displacement(self, i):
-        dr=self.displacement(i)
-        dt=self.time(0)-self.time(-1)
-        dr[0]-=self.old_positions_dict[i][1]*dt
-        dr[3]=math.sqrt(dr[0]*dr[0]+dr[1]*dr[1]+dr[2]*dr[2])
-        return dr
-
-    def instant_relative_velocity(self,i,j):
-        i=str(i)
-        j=str(j)
-
-        od=self.pos_diff_vec(self.old_positions_dict[i],self.old_positions_dict[j])
-        nd=self.pos_diff_vec(self.positions_dict[i],self.positions_dict[j])
-        v=[0.,0.,0.,0.]
-        dt=self.time()-self.time(-1)
-        for s in range(4):
-            v[s]=(nd[s]-od[s])/dt
-        return v
-
-    def average_relative_velocity(self,i,j,avg_time, previous_position):
-        i=str(i)
-        j=str(j)
-        
-        od=self.pos_diff_vec(previous_position[i],previous_position[j])
-        nd=self.pos_diff(i,j)
-#        print i, j, od, nd
-
-        v=[0.,0.,0.,0.]
-        dt=avg_time
-        for s in range(4):
-            v[s]=(nd[s]-od[s])/dt
-        return v
-            
-#end of pos_diff
-
-    def velocity(self,i):
-        
-        i=str(i)
-        #        print pos_diff_vec(positions_dict[i],old_positions_dict[i]) 
-        dt=self.time(0)-self.time(-1)
-        dr=self.pos_diff_vec(self.positions_dict[i],self.old_positions_dict[i])
-        dxinf=self.positions_dict[i][1]*dt # == gamma_dot*y*dt
-        dr[0]-=dxinf
-        return [ dr[j]/dt for j in range(4) ]
 
 
     def goto_time(self, time):
@@ -359,32 +244,8 @@ class Pos_Stream:
         tl_loc=self.current_time_index+t
         return self.time_labels[tl_loc][0]
 
-
-    def np(self):
-        return self.N
-
     def rho(self):
         return self.N/self.V
 
-    def vol(self):
-        return self.V
-
-    def Lx(self):
-        return self.lx
-
-    def Ly(self):
-        return self.ly
-
-    def Lz(self):
-        return self.lz
-
     def range(self, u=0):
-        return range(u,self.np())
-#     #    def range(self, u=1):
-#     #    return range(u,self.part_nb()+1)
-
-    # def is_present(self, i):
-    #     return (i in self.positions_dict)
-    
-    def dimension(self):
-        return self.dim
+        return range(u,self.N)
