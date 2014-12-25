237e920e8ba4a5b68faf5dcc81179486361ce8f3
Ryohei Seto <setoryohei@me.com>
2014-12-25 10:50:08 +0900

    energy dissipation in yaplot expression (unfinished).

--- LF_DEM/VersionInfo.h
+++ LF_DEM/VersionInfo.h
@@ -1,1 +1,1 @@
-#define GIT_VERSION "v1.0-239-g099af61-dirty"
+#define GIT_VERSION "v1.0-240-g983cfed-dirty"
--- LF_DEM/generateYaplotFile.pl
+++ LF_DEM/generateYaplotFile.pl
@@ -7,13 +7,13 @@
 # y_section: Visualize the trimed range of the y coordinates.
 
 use Math::Trig;
-
-$force_factor = 0.000001;
+use IO::Handle;
 
 $y_section = 0;
 $yap_radius = 1;
 
 $particle_data = $ARGV[0];
+
 if ($#ARGV >= 1){
 	$force_factor = $ARGV[1];
 }
@@ -27,19 +27,21 @@ $i = index($particle_data, 'par_', 0)+4;
 $j = index($particle_data, '.dat', $i-1);
 $name = substr($particle_data, $i, $j-$i);
 
-
 $interaction_data = "int_${name}.dat";
-printf "$interaction_data\n";
+#printf "$interaction_data\n";
 $output = "y_$name.yap";
-$output2 = "nvec_$name.dat";
-$out_gaps = "gaps_$name.dat";
-$out_mathm_pos = "mpos_$name.dat";
-$out_mathm_int = "mint_$name.dat";
+$outputed = "ed_$name.dat";
+#$output2 = "nvec_$name.dat";
+#$outenergy = "energy_$name.dat";
+#$out_gaps = "gaps_$name.dat";
+#$out_mathm_pos = "mpos_$name.dat";
+#$out_mathm_int = "mint_$name.dat";
 
 open (IN_rheo, "< rheo_${name}.dat");
 
 ${sum_fmax} = 0;
 ${cnt} = 0;
+
 while (1) {
 	$line = <IN_rheo>;
 	($d1, $d2, $d3, $d4, $d5, $d6, $d7, $d8, $d9, $d10,
@@ -56,16 +58,15 @@ while (1) {
 #$fmax_ave = ${sum_fmax}/${cnt};
 #printf "fmax = $fmax_ave $cnt \n";
 #$force_factor = 0.3/$fmax_ave;
-$force_factor = 0.0003;
-#$force_factor = 0.003;
 #printf  "$fmax_ave\n";
-#exit;
+#exit;$
+
+open (OUTE, "> $outputed");
+
+OUTE->autoflush(1);
 
 open (OUT, "> ${output}");
-open (OUT2, "> ${output2}");
-open (OUTG, "> ${out_gaps}");
-open (OUTMP, "> ${out_mathm_pos}");
-open (OUTMI, "> ${out_mathm_int}");
+
 open (IN_particle, "< ${particle_data}");
 open (IN_interaction, "< ${interaction_data}");
 &readHeader;
@@ -73,22 +74,72 @@ $first=1;
 $c_traj=0;
 $num = 0;
 
+printf OUT "\@0 0 0 0 \n";
+printf OUT "\@1 50 100 205 \n";
+printf OUT "\@2 100 100 100 \n";
+printf OUT "\@3 50 150 255 \n";
+printf OUT "\@4 50 200 50 \n";
+printf OUT "\@5 255 100 100 \n";
+printf OUT "\@6 50 200 50 \n";
+printf OUT "\@7 200 200 200 \n";
+printf OUT "\@8 0 70 50 \n";
+printf OUT "\@10 250 250 250 \n";
+printf OUT "\@11 240 240 240 \n";
+printf OUT "\@12 230 230 230 \n";
+printf OUT "\@13 220 220 220 \n";
+printf OUT "\@14 210 210 210 \n";
+printf OUT "\@15 200 200 200 \n";
+printf OUT "\@16 190 190 190 \n";
+printf OUT "\@17 180 180 180 \n";
+printf OUT "\@18 170 170 170 \n";
+printf OUT "\@19 160 160 160 \n";
+printf OUT "\@20 150 150 150 \n";
+printf OUT "\@21 140 140 140 \n";
+printf OUT "\@22 130 130 130 \n";
+printf OUT "\@23 120 120 120 \n";
+printf OUT "\@24 110 110 110 \n";
+printf OUT "\@25 100 100 100 \n";
+printf OUT "\@26 90 90 90 \n";
+printf OUT "\@27 80 90 90 \n";
+printf OUT "\@28 70 70 70\n";
+printf OUT "\@29 60 60 60 \n";
+printf OUT "\@30 50 50 50 \n";
+printf OUT "\@31 40 40 40 \n";
+printf OUT "\@32 30 30 30 \n";
+printf OUT "\@33 20 20 20 \n";
+printf OUT "\@34 10 10 10 \n";
+printf OUT "\@35 0 0 0 \n";
+
+
+$total_energy_dis = 0;
+$shear_strain_next = 1;
+$shear_strain_previous = 0;
 while (1){
-	
 	&InParticles;
-	
 	&InInteractions;
-	
-	printf "$shear_rate $shear_stress\n";
+
 	last unless defined $line;
 	&OutYaplotData;
+	
+	$total_energy = ($shear_stress)*($shear_strain-$shear_strain_previous )*102.636;
+	$energy_diff = $total_energy_dis - $total_energy;
+	if ($shear_strain > $shear_strain_next ) {
+		$shear_strain_next += 0.1;
+		printf "$shear_strain $total_energy_dis $total_energy $energy_diff  $shear_stress\n";
+		printf OUTE "$shear_strain $total_energy_dis $total_energy $energy_diff  $shear_stress\n";
+		$total_energy_dis = 0;
+		$shear_strain_previous = $shear_strain;
+	}
+	
 	$num ++;
-
 }
+
 close (OUT);
+close (OUTE);
+close (IN_particle);
+close (IN_interaction);
 
 sub readHeader{
-	
 	$line = <IN_particle>;
 	$line = <IN_particle>; ($buf, $buf, $np) = split(/\s+/, $line);
 	$line = <IN_particle>; ($buf, $buf, $VF) = split(/\s+/, $line);
@@ -103,13 +154,17 @@ sub readHeader{
 	for ($i = 0; $i < 24; $i++) {
 		$line = <IN_interaction>;
 	}
-	printf "$np, $VF, $Lx, $Ly, $Lz\n";
+	#printf "$np, $VF, $Lx, $Ly, $Lz\n";
 }
 
 sub InParticles {
 	$radius_max = 0;
 	$line = <IN_particle>;
     ($buf, $shear_strain, $shear_disp, $shear_rate, $shear_stress) = split(/\s+/, $line);
+	# shear_rate/shear_rate0
+	# shear_rate0 = Fr(0)/(6 pi eta0 a) = 1/6pi
+
+	$shear_rate = $shear_rate;
 	
 	# h_xzstress << sp << c_xzstressXF << sp << c_xzstressGU << sp << b_xzstress
 	# 1: number of the particle
@@ -127,15 +182,22 @@ sub InParticles {
         ($ip, $a, $x, $y, $z, $vx, $vy, $vz, $ox, $oy, $oz,
 		$h_xzstress, $c_xzstressGU, $b_xzstress, $angle) = split(/\s+/, $line);
 		
-		if (true){
-			#printf OUTMP "$line";
-			printf OUTMP "$i $x $y $z $a\n";
-		}
+#		if (true){
+#			#printf OUTMP "$line";
+#			printf OUTMP "$i $x $y $z $a\n";
+#		}
 		
 		$radius[$i] = $a;
         $posx[$i] = $x;
         $posy[$i] = $y;
         $posz[$i] = $z;
+		$velx[$i] = $vx;
+		$vely[$i] = $vy;
+		$velz[$i] = $vz;
+		$omegax[$i] = $ox;
+		$omegay[$i] = $oy;
+		$omegaz[$i] = $oz;
+		
 		$omegay[$i] = $oy;
 		$ang[$i] = $angle;
 		if ($radius_max < $a){
@@ -214,6 +276,9 @@ sub InInteractions {
 	# 13: N1 contribution of contact xF
 	# 14: N2 contribution of contact xF
 	
+	$velocity0 = $shear_rate;
+	$force0 = 6*pi*($shear_rate);
+	
 	for ($k = 0; $k < $num_interaction; $k ++){
 		$line = <IN_interaction> ;
 		#		"#1: particle 1 label\n"
@@ -240,20 +305,46 @@ sub InInteractions {
 		$fc_tan_x, $fc_tan_y, $fc_tan_z, # 13, 14, 15
 		$fr_norm, $s_xF
 		) = split(/\s+/, $line);
-		if ($num==$num_mathm){
-			printf OUTMI "$line";
-		}
 		# $F_lub[$k] + $Fc_n[$k] + $Fcol[$k];
 		$int0[$k] = $i;
 		$int1[$k] = $j;
 		$contactstate[$k] = $contact;
+		$vx1 = $velocity0*($velx[$i] + $radius[$i]*($ny*$omegaz[$i]-$nz*$omegay[$i]));
+		$vy1 = $velocity0*($vely[$i] + $radius[$i]*($nz*$omegax[$i]-$nx*$omegaz[$i]));
+		$vz1 = $velocity0*($velz[$i] + $radius[$i]*($nx*$omegay[$i]-$ny*$omegax[$i]));
+		$vx2 = $velocity0*($velx[$j] - $radius[$j]*($ny*$omegaz[$j]-$nz*$omegay[$j]));
+		$vy2 = $velocity0*($vely[$j] - $radius[$j]*($nz*$omegax[$j]-$nx*$omegaz[$j]));
+		$vz2 = $velocity0*($velz[$j] - $radius[$j]*($nx*$omegay[$j]-$ny*$omegax[$j]));
+		$rvelx[$k] = ($vx2 - $vx1);
+		$rvely[$k] = ($vy2 - $vy1);
+		$rvelz[$k] = ($vz2 - $vz1);
+		$rvel_dot_norm = $rvelx[$k]*$nx + $rvely[$k]*$ny + $rvelz[$k]*$nz;
+		$rvelx[$k] -= $rvel_dot_norm*$nx;
+		$rvely[$k] -= $rvel_dot_norm*$ny;
+		$rvelz[$k] -= $rvel_dot_norm*$nz;
 		$domega[$k] = $omegay[$i] - $omegay[$j];
+		$f_normal = $fc_norm +$f_lub_norm + $fr_norm;
+#		$fx[$k] = $force0*($fc_tan_x + $f_lub_tan_x + $f_normal*$nx);
+#		$fy[$k] = $force0*($fc_tan_y + $f_lub_tan_y + $f_normal*$ny);
+#		$fz[$k] = $force0*($fc_tan_z + $f_lub_tan_z + $f_normal*$nz);
+		#		$fx[$k] = $force0*($fc_tan_x + $f_lub_tan_x);
+		#$fy[$k] = $force0*($fc_tan_y + $f_lub_tan_y);
+		#$fz[$k] = $force0*($fc_tan_z + $f_lub_tan_z);
+		$fx[$k] = $force0*($f_lub_tan_x);
+		$fy[$k] = $force0*($f_lub_tan_y);
+		$fz[$k] = $force0*($f_lub_tan_z);
+
+		$energy_dis[$k] = ($rvelx[$k]*$fx[$k] + $rvely[$k]*$fy[$k] + $rvelz[$k]*$fz[$k]);
+		$total_energy_dis += $energy_dis[$k];
+		#$fx[$k] = $f_lub_tan_x; #+ $f_normal*$nx;
+		#$fy[$k] = $f_lub_tan_y;# + $f_normal*$ny;
+		#$fz[$k] = $f_lub_tan_z;#+ $f_normal*$nz;
 		$F_lub[$k] = $f_lub_norm;
 		$Fc_n[$k] = $fc_norm;
 		$Fc_t[$k] = sqrt($fc_tan_x**2+$fc_tan_y**2+$fc_tan_z**2);
 		#$f_normal = $fc_norm + $fr_norm + $f_lub_norm;
 		#$f_normal = $f_lub_norm;
-		$f_normal = $fc_norm;
+
 		$force[$k] = $f_normal;
 
 		$S_bf[$k] =  $s_xF;
@@ -266,13 +357,13 @@ sub InInteractions {
 		$ft_z[$k] = $f_lub_tan_z + $fc_tan_z;
 		
 		$Gap[$k] = $gap;
-		printf OUTG "$gap ";
+		#	printf OUTG "$gap ";
 	}
-	printf OUTG "\n";
+	
+	#printf OUTG "\n";
 }
 
 sub OutYaplotData{
-	
 	if ($first == 0){
 		printf OUT "\n";
 	} else {
@@ -282,24 +373,18 @@ sub OutYaplotData{
 	$postext2 = $Lz/2+3;
 	$shear_rate_text = int($shear_rate*1e5);
 	$shear_rate_text *= 1e-5;
-
-	
 	#printf OUT "y 10\n";
 	#printf OUT "@ 2\n";
 	#printf OUT "t -2 0 $postext shear rate = $shear_rate_text \n";
 	#printf OUT "t -2 0 $postext2 shear stress = $shear_stress \n";
-	
 	#$meterx = $Lx/2+1;
 	#$meterbottom = -$Lz/2;
 	#$metertop = $meterbottom + $shear_rate*1000;
 	printf OUT "@ 3\n";
-	&OutMeter($shear_rate, 0.01, $Lx/2+2, -$Lz/2+2);
-	printf OUT "@ 4\n";
+	&OutMeter($shear_rate, 0.02, $Lx/2+2, -$Lz/2+2);
+	#printf OUT "@ 4\n";
 	#&OutMeter($shear_stress, 2, -$Lx/2-3.5, -$Lz/2+2);
-	&OutStress($shear_stress, 5);
-
-
-	
+	#&OutStress($shear_stress, 5);
 	#
 	#	printf OUT "y 7\n";
 	#	printf OUT "r 0.1\n";
@@ -359,7 +444,7 @@ sub OutYaplotData{
 	#	}
 	
 	printf OUT "y 1\n";
-    printf OUT "@ 10\n";
+    printf OUT "@ 8\n";
 	$r = $yap_radius*$radius[0];
 	printf OUT "r $r\n";
     for ($i = 0; $i < $np; $i ++){
@@ -400,16 +485,18 @@ sub OutYaplotData{
 #
 
 	
-	printf OUT "y 2\n";
-	printf OUT "r 0.2\n";
-
-	printf OUT "@ 5\n"; # static
-	for ($k = 0; $k < $num_interaction; $k ++){
-		if ($contactstate[$k] > 1) {
-			&OutString2($int0[$k],  $int1[$k]);
-			#&OutContact($int0[$k], $int1[$k], $contactstate[$k]);
-		}
-	}
+#	printf OUT "y 2\n";
+#	
+#	
+#	printf OUT "r 0.2\n";
+#
+#	printf OUT "@ 5\n"; # static
+#	for ($k = 0; $k < $num_interaction; $k ++){
+#		if ($contactstate[$k] > 1) {
+#			&OutString2($int0[$k],  $int1[$k]);
+#			#&OutContact($int0[$k], $int1[$k], $contactstate[$k]);
+#		}
+#	}
 	#
 	#
 #		printf OUT "y 4\n";
@@ -428,26 +515,26 @@ sub OutYaplotData{
 #							$zz = 0.5*($posz[$int0[$k]] + $posz[$int1[$k]]);
 #							$tmpoy = int $oy;
 #							printf OUT "t $xx -0.2 $zz $tmpoy \n";
-	#					}
-	#
-	#			}
-	#		}
-	#    }
-	#		printf OUT "y 3\n";
-	#		printf OUT "r 0.4\n";
-	#		printf OUT "@ 7\n"; # static
-	#		for ($k = 0; $k < $num_interaction; $k ++){
-	#			$force = $Fc_n[$k];
-	#	        if ($F_lub[$k] < 0) {
-	#				$force += - $F_lub[$k];
-	#			}
-	#			if ($Gap[$k] < 0) {
-	#				if ($fricstate[$k] == 0) {
-	#					&OutString2($int0[$k],  $int1[$k]);
-	#				}
-	#			}
-	#	    }
-	#
+#						}
+#	
+#				}
+#			}
+#	    }
+#			printf OUT "y 3\n";
+#			printf OUT "r 0.4\n";
+#			printf OUT "@ 7\n"; # static
+#			for ($k = 0; $k < $num_interaction; $k ++){
+#				$force = $Fc_n[$k];
+#		        if ($F_lub[$k] < 0) {
+#					$force += - $F_lub[$k];
+#				}
+#				if ($Gap[$k] < 0) {
+#					if ($fricstate[$k] == 0) {
+#						&OutString2($int0[$k],  $int1[$k]);
+#					}
+#				}
+#		    }
+#	
 	
 	#	printf OUT "y 9\n";
 	#	printf OUT "r 0.35\n";
@@ -480,30 +567,41 @@ sub OutYaplotData{
 	
 	
 	#printf OUT "r 0.2\n";
-	$force_factor = 0.05;
-	printf OUT "y 3\n";
-	printf OUT "@ 3\n";
-	for ($k=0; $k<$num_interaction; $k++){
-		#$force = $F_lub[$k] + $Fc_n[$k] + $Fcol[$k];
-		#$force = $Fcol[$k];
-		#$force = $Fc_n[$k];
-		if ($force[$k] < 0){
-			$force = -$force[$k];
-			$string_width = ${force_factor}*${force};
-			#&OutString2($int0[$k], $int1[$k]);
-			&OutString_width($int0[$k], $int1[$k]);
-		}
-	}
+#	$force_factor = 0.05*$shear_stress;
+#	printf OUT "y 3\n";
+#	printf OUT "@ 3\n";
+#	for ($k=0; $k<$num_interaction; $k++){
+#		#$force = $F_lub[$k] + $Fc_n[$k] + $Fcol[$k];
+#		#$force = $Fcol[$k];
+#		#$force = $Fc_n[$k];
+#		if ($force[$k] < 0){
+#			$force = -$force[$k];
+#			$string_width = ${force_factor}*${force};
+#			#&OutString2($int0[$k], $int1[$k]);
+#			&OutString_width($int0[$k], $int1[$k]);
+#		}
+#	}
+#	printf OUT "y 4\n";
+#	printf OUT "@ 4\n";
+#	for ($k = 0; $k < $num_interaction; $k ++){
+#		if ($force[$k] > 0){
+#			$force = $force[$k];
+#			$string_width = ${force_factor}*${force};
+#			#&OutString2($int0[$k], $int1[$k]);
+#			&OutString_width($int0[$k], $int1[$k]);
+#		}
+#	}
+	
 	printf OUT "y 4\n";
 	printf OUT "@ 4\n";
+	printf OUT "r 0.5\n";
 	for ($k = 0; $k < $num_interaction; $k ++){
-		if ($force[$k] > 0){
-			$force = $force[$k];
-			$string_width = ${force_factor}*${force};
-			#&OutString2($int0[$k], $int1[$k]);
-			&OutString_width($int0[$k], $int1[$k]);
-		}
+		&OutEnergyDissipation($int0[$k], $int1[$k], $k);
 	}
+	
+	
+	
+	
 	#	printf OUT "y 2\n";
 #	printf OUT "@ 6\n";
 #	printf OUT "r 0.2\n";
@@ -580,13 +678,13 @@ sub OutYaplotData{
 	#    }
 	
 
-#	if ($Ly == 0){
-#		printf OUT "y 6\n";
-#		printf OUT "@ 1\n";
-#		for ($i = 0; $i < $np; $i ++){
-#			&OutCross($i);
-#		}
-#	}
+	if ($Ly == 0){
+		printf OUT "y 6\n";
+		printf OUT "@ 1\n";
+		for ($i = 0; $i < $np; $i ++){
+			&OutCross($i);
+		}
+	}
 	
 	&OutBoundaryBox;
 	
@@ -683,6 +781,61 @@ sub OutNvec {
 	$nz = $nrvec_z[$k];
 	printf OUT2 "$nx $ny $nz\n";
 }
+
+sub OutEnergyDissipation {
+	($i, $j, $k) = @_;
+	$xi = $posx[$i];
+	$yi = $posy[$i] - 0.01;
+	$zi = $posz[$i];
+	$xj = $posx[$j];
+	$yj = $posy[$j] - 0.01;
+	$zj = $posz[$j];
+	$velfactor = 0.05;
+	$sq_dist = ($xi-$xj)**2 + ($yi-$yj)**2 + ($zi-$zj)**2 ;
+	if (sqrt($sq_dist) < $radius[$i] + $radius[$j]+1){
+		$xs = ($radius[$j]*$xi + $radius[$i]*$xj)/($radius[$j]+$radius[$i]);
+		$ys = ($radius[$j]*$yi + $radius[$i]*$yj)/($radius[$j]+$radius[$i]);
+		$zs = ($radius[$j]*$zi + $radius[$i]*$zj)/($radius[$j]+$radius[$i]);
+		$xe = $xs + $velfactor*$rvelx[$k];
+		$ye = $ys + $velfactor*$rvely[$k];
+		$ze = $zs + $velfactor*$rvelz[$k];
+		$xe2 = $xi + $radius[$i]*$nrvec_x[$k];
+		$ye2 = $yi + $radius[$i]*$nrvec_y[$k];
+		$ze2 = $zi + $radius[$i]*$nrvec_z[$k];
+		#$energydis = abs($rvelx[$k]*$fx[$k] + $rvely[$k]*$fy[$k] + $rvelz[$k]*$fz[$k]);
+		#		if ($shear_strain > 0.5) {
+		#			printf "$energydis\n";
+		#		}
+		$energydis = 5000*$energy_dis[$k];
+		if ($energydis > 1){
+			$energydisINT = int ($energydis);
+			#printf "$energydisINT\n";
+			$energyColor = 35 - $energydisINT ;
+			if ($energyColor < 10){
+				#printf "$energydisINT\n";
+				$energyColor = 10;
+			}
+		} else {
+			$energyColor = 35;
+		}
+		#		printf OUT "r 0.1\n";
+		#		printf OUT "s $xs $ys $zs $xe $ye $ze\n";
+		#		printf OUT "s $xi $yi $zi $xe2 $ye2 $ze2\n";
+		if ($contactstate[$k] == 2){
+			#printf OUT "@ 35\n";
+			#printf OUT "c $xs $ys $zs \n";
+		} else {
+			if ($energydisINT != 0) {
+				#printf OUT "r $energydis \n";
+				printf OUT "@ $energyColor\n";
+				printf OUT "c $xs $ys $zs \n";
+			}
+		}
+	}
+}
+
+
+
 
 sub OutString_width {
     ($i, $j) = @_;
