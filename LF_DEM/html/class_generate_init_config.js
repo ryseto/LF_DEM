436ceac68da780e24698172b66e2504f118a2059
Romain Mari <romain.mari@gmail.com>
2015-02-10 18:31:52 -0500

    rm docs

--- LF_DEM/html/class_generate_init_config.js
+++
@@ -1,5 +0,0 @@
-var class_generate_init_config =
-[
-    [ "GenerateInitConfig", "class_generate_init_config.html#ab9990a0076f80cde90a1ee562e41eeee", null ],
-    [ "generate", "class_generate_init_config.html#a71997d8fa1ef8e4dd663a7401fe90bee", null ]
-];
--- LF_DEM/html/search/functions_75.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['update',['update',['../class_averager.html#ae5f0d55419b26b218cb16c7ce1c3dbf3',1,'Averager']]]
-];
