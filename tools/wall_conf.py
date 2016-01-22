045022e687cc6767267fc655508ba8edcfd9c4f5
Romain Mari <romain.mari@gmail.com>
2016-01-22 17:39:30 +0000

    wall_conf.py stores wall velocities

--- tools/wall_conf.py
+++ tools/wall_conf.py
@@ -21,29 +21,35 @@ fixed_part = np.logical_or(bottom_particles, top_particles)
 # now trick LF_DEM PBCs along z by extending the system along z
 # and putting particles in the middle of this extended box,
 zextension = 4*np.amax(rad)
-print(meta['lz'])
 meta['lz'] = str(float(meta['lz']) + zextension)
 pos[:,2] += zextension/2
-print(meta['lz'])
 
 # separate frozen from mobile particles
-fixed_pos = pos[fixed_part]
 fixed_rad = rad[fixed_part]
 mobile_pos = pos[np.logical_not(fixed_part)]
 mobile_rad = rad[np.logical_not(fixed_part)]
 mobile = np.column_stack((mobile_pos, mobile_rad))
-out_data = mobile
-fixed = np.column_stack((fixed_pos, fixed_rad))
-out_data = np.row_stack((out_data, fixed))
+# out_data = mobile
+fixed_pos = pos[fixed_part]
+fixed_vel = np.zeros(pos.shape)
+fixed_vel[bottom_particles] = np.array([-1, 0, 0])
+fixed_vel[top_particles] = np.array([1, 0, 0])
+fixed_vel = fixed_vel[fixed_part]
+fixed = np.column_stack((fixed_pos, fixed_rad, fixed_vel))
+
+# out_data = np.row_stack((out_data, fixed))
 
 # output all that
 out_fname = fname.replace(".dat", "walls.dat")
 with open(out_fname, "w") as out:
-    line1 = '# '+' '.join(meta.keys())+'\n'
-    line2 = '# '+' '.join(meta.values())+'\n'
+    line1 = '# '+' '.join(meta.keys())+' np_fixed\n'
+    line2 = '# '+' '.join(meta.values())+' '+str(len(fixed_pos))+'\n'
+    # print(line2)
     out.write(line1)
     out.write(line2)
 
 out = open(out_fname, "ab")
-np.savetxt(out, out_data)
+np.savetxt(out, mobile)
+np.savetxt(out, fixed)
+
 out.close()
