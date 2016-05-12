c7b6636f087bc6553ce9982a8851b21044a96724
Ryohei Seto <seto@msmmacpro1.local>
2016-05-12 11:31:05 +0900

    Adapt visualization script to new snapshot header of interaction file.

--- tools/generateYaplotFile.pl
+++ tools/generateYaplotFile.pl
@@ -52,6 +52,7 @@ open (IN_interaction, "< ${interaction_data}");
 
 $cnt_interval = 0;
 $first = 1;
+$first_int = 1;
 $checkpoint = 1;
 $shear_strain_previous = 0;
 $shearrate_positive = 1;
@@ -115,12 +116,15 @@ sub readHeader {
 	$line = <IN_particle>; ($buf, $buf, $Lx) = split(/\s+/, $line);
 	$line = <IN_particle>; ($buf, $buf, $Ly) = split(/\s+/, $line);
 	$line = <IN_particle>; ($buf, $buf, $Lz) = split(/\s+/, $line);
-	for ($i = 0; $i<16; $i++) {
+	for ($i = 0; $i<8; $i++) {
 		$line = <IN_particle>;
+		printf "$line";
 	}
-	for ($i = 0; $i<24; $i++) {
+	for ($i = 0; $i<19; $i++) {
 		$line = <IN_interaction>;
+		printf "$line";
 	}
+	printf "=====\n";
 }
 
 sub yaplotColor {
@@ -180,7 +184,7 @@ sub InParticles {
 		# 5 sys.get_time()
 		# 6 sys.angle_external_magnetic_field
 		($buf, $shear_strain, $shear_disp, $shear_rate, $shear_stress) = split(/\s+/, $line);
-
+		printf "$shear_strain\n";
 		for ($i = 0; $i < $np; $i ++){
 			$line = <IN_particle>;
 			if ($output == 1) {
@@ -227,12 +231,20 @@ sub InParticles {
 	}
 }
 
-sub InInteractions {
-	$line = <IN_interaction>;
-	($buf, $shear_strain_i, $num_interaction) = split(/\s+/, $line);
+sub InInteractions{
+	#	$line = <IN_interaction>;
+	#($buf, $shear_strain_i, $num_interaction) = split(/\s+/, $line);
+	#printf "int $buf $shear_strain_i $num_interaction\n";
+	
+	if ($first_int == 1) {
+		$first_int = 0;
+		$line = <IN_interaction>;
+		($buf, $buf1, $buf2, $buf3, $buf4) = split(/\s+/, $line);
+	}
 	if ($buf != "#") {
 		exit(1);
 	}
+	
 	# 1, 2: numbers of the interacting particles
 	# 3: 1=contact, 0=apart
 	# 4, 5, 6: normal vector
@@ -244,8 +256,17 @@ sub InInteractions {
 	# 12: Viscosity contribution of contact xF
 	# 13: N1 contribution of contact xF
 	# 14: N2 contribution of contact xF
-	for ($k = 0; $k < $num_interaction; $k ++){
+	while (true) {
 		$line = <IN_interaction>;
+		($i, $j, $contact, $nx, $ny, $nz, #1---6
+		$gap, $f_lub_norm, # 7, 8
+		$f_lub_tan_x, $f_lub_tan_y, $f_lub_tan_z, # 9, 10, 11
+		$fc_norm, # 12
+		$fc_tan_x, $fc_tan_y, $fc_tan_z, # 13, 14, 15
+		$fr_norm, $s_xF) = split(/\s+/, $line);
+		if ($i == "#") {
+			last;
+		}
 		#		"#1: particle 1 label\n"
 		#		"#2: particle 2 label\n"
 		#		"#3: contact state (0 = no contact, 1 = frictionless contact, 1 = non-sliding frictional, 2 = sliding frictional)\n"
@@ -264,12 +285,6 @@ sub InInteractions {
 		#		"#16: norm of the normal repulsive force\n"
 		#		"#17: Viscosity contribution of contact xF\n";
 		if ($output == 1) {
-			($i, $j, $contact, $nx, $ny, $nz, #1---6
-			$gap, $f_lub_norm, # 7, 8
-			$f_lub_tan_x, $f_lub_tan_y, $f_lub_tan_z, # 9, 10, 11
-			$fc_norm, # 12
-			$fc_tan_x, $fc_tan_y, $fc_tan_z, # 13, 14, 15
-			$fr_norm, $s_xF) = split(/\s+/, $line);
 			$int0[$k] = $i;
 			$int1[$k] = $j;
 			$contactstate[$k] = $contact;
