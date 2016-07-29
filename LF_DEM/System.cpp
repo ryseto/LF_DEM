a84707e904abb3983023994de300e7d6508c5437
Romain Mari <romain.mari@gmail.com>
2016-07-29 14:49:25 +0100

    changing hash to wagnerhash when using DSFMT to avoid std::hash clash

--- LF_DEM/System.cpp
+++ LF_DEM/System.cpp
@@ -17,7 +17,41 @@
 #define GRANDOM  ( sqrt( -2.0 * log( 1.0 - dsfmt_genrand_open_open(&r_gen) ) ) * cos(2.0 * 3.14159265358979323846264338328 * dsfmt_genrand_close_open(&r_gen) ) ) // RNG gaussian with mean 0. and variance 1.
 #endif
 
+
 using namespace std;
+
+#ifdef USE_DSFMT
+inline unsigned long
+wagnerhash(time_t t, clock_t c)
+{
+	/**
+		\brief Utility function to start up the DSFMT RNG with a nice seed.
+
+	 From MersenneTwister v1.0 by Richard J. Wagner
+	 comments below are from the original code.
+
+	 Get a unsigned long from t and c
+	 Better than unsigned long(x) in case x is floating point in [0,1]
+	 Based on code by Lawrence Kirby (fred@genesis.demon.co.uk)
+	*/
+
+	static unsigned long differ = 0; // guarantee time-based seeds will change
+	unsigned long h1 = 0;
+	unsigned char *pp = (unsigned char *) &t;
+	for (size_t i=0; i<sizeof(t); ++i){
+		h1 *= UCHAR_MAX + 2U;
+		h1 += pp[i];
+	}
+	unsigned long h2 = 0;
+	pp = (unsigned char *) &c;
+	for (size_t j=0; j<sizeof(c); ++j) {
+		h2 *= UCHAR_MAX + 2U;
+		h2 += pp[j];
+	}
+	return (h1 + differ++)^h2;
+}
+#endif
+
 
 System::System(ParameterSet& ps, list <Event>& ev):
 events(ev),
@@ -423,7 +457,7 @@ void System::setupSystemPreConfiguration(string control, bool is2d)
 		r_gen = new MTRand;
 #endif
 #ifdef USE_DSFMT
-		dsfmt_init_gen_rand(&r_gen, hash(std::time(NULL), clock()) ) ; // hash of time and clock trick from MersenneTwister v1.0 by Richard J. Wagner
+		dsfmt_init_gen_rand(&r_gen, wagnerhash(std::time(NULL), clock()) ) ; // hash of time and clock trick from MersenneTwister v1.0 by Richard J. Wagner
 #endif
 #endif
 	}
