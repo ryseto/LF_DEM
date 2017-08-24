ee39c0f6401b27c31e8837aa65460b8fc7da8d6a
Romain Mari <romain.mari@univ-grenoble-alpes.fr>
2017-08-24 10:44:18 +0200

    config UGA

---
+++ LF_DEM/config/Makefile_config_uga_gpu.mk
@@ -0,0 +1,41 @@
+#=======================================#
+#    Parameters to provide              #
+#=======================================#
+
+
+# Use DSFMT instead of MT as a RNG ( yes / no )
+DSFMT_RNG = no
+# Enable use of Metis library ( yes / no )
+UseMetis = yes
+
+# the directory where LF_DEM will be copied on `make install`
+install_dir = ~/bin/
+
+# C++ compiler
+CXX = g++
+
+# Libraries
+#
+# SuiteSparse library install folder
+SUITESPARSE_ROOT = /home/romain/src/SuiteSparse/
+
+
+# Extra flags to the compiler, if needed (e.g. optimization flags)
+CXXFLAGS_EXTRA = -L${CUDA_HOME}/lib -lcuda -L/usr/lib64/nvidia/
+Cholmod_GPU_libpath = $(SUITESPARSE_ROOT)/lib/
+Cholmod_GPU = $(Cholmod_GPU_libpath)libSuiteSparse_GPURuntime.so $(Cholmod_GPU_libpath)libGPUQREngine.so
+
+
+#======== Linking ==================
+
+# Blas and Lapack (you may want to modify the BLAS, as -lblas might point to non optimized BLAS)
+#Blas_Linking_Flags = -lblas
+#Lapack_Linking_Flags = -llapack
+# Intel MKL
+Blas_Linking_Flags = -lopenblas
+Lapack_Linking_Flags = -llapack
+
+# Extra linking here, if needed
+Extra_Linking_Flags =
+# for OpenMP with g++
+# Extra_Linking_Flags = -fopenmp
---
+++ tools/config/Makefile_config_uga_Romain.mk
@@ -0,0 +1,2 @@
+INSTALL_DIR = ~/bin/
+ANACONDA_DIR = ~/anaconda3/lib/python3.6/site-packages
