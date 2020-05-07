00bfd3ada71200096aade1e4b72a599f2115f575
Romain Mari <romain.mari@univ-grenoble-alpes.fr>
2020-05-07 15:36:18 +0200

    simplifying build in pyLF_DEM

--- pyLF_DEM/setup.py
+++ pyLF_DEM/setup.py
@@ -7,30 +7,52 @@ setup.py file for pyLF_DEM
 from distutils.core import setup, Extension
 import os
 import subprocess
-import setup_in
 
-os.environ["CC"] = setup_in.cc
-os.environ["CXX"] = setup_in.cxx
+import re
 
-extra_link_args = setup_in.Blas_Linking_Flags + setup_in.Lapack_Linking_Flags\
-                  + setup_in.SuiteSparse_Linking_Flags + setup_in.Extra_Linking_Flags
+LFDEM_ROOT = "../"
+
+# find all variables defines in Makefile_config.mk
+mkconfig = {}
+with open(LFDEM_ROOT+"LF_DEM/config/Makefile_config.mk") as f:
+    for line in f:
+        line = line.strip()
+        if len(line) and line[0] != '#':
+            k, val = line.split(' =', maxsplit=1)
+            mkconfig[k] = val.lstrip().rstrip()
+
+# replace all the ${...} by their value
+for k in mkconfig:
+    for m in re.finditer('\$\{\w*\}', mkconfig[k]):
+         mkconfig[k] = mkconfig[k].replace(m.group(0), mkconfig[m.group(0)[2:-1]])
+         
+
+os.environ["CC"] = mkconfig['CC']
+os.environ["CXX"] = mkconfig['CXX']
+
+
+extra_link_args = mkconfig['Blas_Linking_Flags'].split()
+extra_link_args += [mkconfig['Lapack_Linking_Flags']]
+
+SUITESPARSE_ROOT = mkconfig['SUITESPARSE_ROOT']
+extra_link_args += [SUITESPARSE_ROOT+"/lib/libcholmod.so",
+                             '-Wl,-rpath='+SUITESPARSE_ROOT+"/lib/"]
 extra_link_args = [a for a in extra_link_args if len(a)]
-print(extra_link_args)
-include_dirs = setup_in.Blas_Include_Dirs + setup_in.Lapack_Include_Dirs\
-               + setup_in.SuiteSparse_Include_Dirs
-extra_compile_args = setup_in.Extra_Compile_Args+["-std=c++11", "-O3", "-fpermissive"] # -fpermissive for gsd.c
+
+include_dirs = [SUITESPARSE_ROOT+"/include/"]
+
+extra_compile_args = ["-std=c++11", "-O3", "-fpermissive"] # -fpermissive for gsd.c
 
 pylf_dem_version = subprocess.check_output(
                    ["git", "describe", "--dirty"])
 pylf_dem_version = pylf_dem_version.decode("utf-8")[:-1]
 
 extra_compile_args += ["-DGIT_VERSION=\""+pylf_dem_version+"\""]
-library_dirs = setup_in.Library_Dirs
 
-srcs = [x for x in os.listdir(setup_in.LFDEM_ROOT+"/LF_DEM/") if x.find(".cpp")+4==len(x)]
-srcs.append(setup_in.LFDEM_ROOT+"/LF_DEM/gsd.c")
+srcs = [x for x in os.listdir(LFDEM_ROOT+"/LF_DEM/") if x.find(".cpp")+4==len(x)]
+srcs.append(LFDEM_ROOT+"/LF_DEM/gsd.c")
 for i in range(len(srcs)):
-    srcs[i] = setup_in.LFDEM_ROOT+"/LF_DEM/"+srcs[i]
+    srcs[i] = LFDEM_ROOT+"/LF_DEM/"+srcs[i]
 srcs.append('pyLFDEM_wrap.cxx')
 
 
@@ -39,7 +61,6 @@ pyLFDEM_module = Extension('_pyLFDEM',
                            language="c++",
                            include_dirs=include_dirs,
                            extra_compile_args=extra_compile_args,
-                           library_dirs=library_dirs,
                            extra_link_args=extra_link_args
                           )
 
